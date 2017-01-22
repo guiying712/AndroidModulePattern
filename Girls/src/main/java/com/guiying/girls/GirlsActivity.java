8b16181baf9fa8faeeb6b355c1d9008f1485ef8a
guiying712 <guiying712@163.com>
2017-01-22 21:18:00 +0800

    创建公共库Common，以及组件News和Girls

---
+++ Girls/src/main/java/com/guiying/girls/GirlsActivity.java
@@ -0,0 +1,13 @@
+package com.guiying.girls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GirlsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_girls);
+    }
+}
